8a1bf1c5cb7744cf450708367b59f5abe734302f
Edmond Leylo <edmondlaylo@gmail.com>
2016-01-12 08:01:44 +0200

    5

--- DemoGo/Main.aspx.cs
+++ DemoGo/Main.aspx.cs
@@ -14,7 +14,7 @@ namespace DemoGo
 
         }
 
-        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        protected void Button1_Click(object sender, EventArgs e)
         {
             Label2.Text = (Convert.ToInt32(Label2.Text) + 1).ToString(); 
         }
